980e8f8477f0d7ee58328103e0e52433200979e5
Rodrigo Azevedo <razevedocosta@gmail.com>
2023-11-04 11:35:48 -0300

    update

--- src/main/java/br/com/alura/screenmatch/model/Categoria.java
+++ src/main/java/br/com/alura/screenmatch/model/Categoria.java
@@ -1,2 +1,25 @@
-package br.com.alura.screenmatch.model;public enum Categoria {
+package br.com.alura.screenmatch.model;
+
+public enum Categoria {
+    AÇÃO("Action"),
+    COMÉDIA("Comedy"),
+    CRIME("Crime"),
+    DRAMA("Drama"),
+    ROMANCE("Romance");
+
+    private String categoriaOmdb;
+
+    Categoria(String categoriaOmdb) {
+        this.categoriaOmdb = categoriaOmdb;
+    }
+
+    public static Categoria fromString(String text) {
+        for (Categoria categoria : Categoria.values()) {
+            if (categoria.categoriaOmdb.equalsIgnoreCase(text)) {
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("Nenhuma categoria encontrada para a string fornecida: " + text);
+    }
+
 }
